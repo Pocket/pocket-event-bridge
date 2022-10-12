9fa6cc99f74209a5ce519379d874f2a04aabf9cc
Herraj Luhano <hluhano@mozilla.com>
2022-10-12 13:47:53 -0400

    feat(dismissProspect): [BACK-1623] add event rule and sns topic for dismiss prospect event

--- .aws/src/event-rules/prospect-events/prospectEventRules.ts
+++ .aws/src/event-rules/prospect-events/prospectEventRules.ts
@@ -12,17 +12,17 @@ import { eventConfig } from './eventConfig';
 
 /**
  * Purpose:
- * 
+ *
  * The purpose of this rule set is to send ML-generated prospects to two
  * separate systems:
- * 
+ *
  * 1. A pre-existing production SQS queue that is consumed by a lambda which
  * feeds those prospects to the curation admin tool.
- * 
+ *
  * 2. The dev instance of this event bridge, which will in turn send the
  * prospects to the dev SQS queue to be consumed by the dev lambda and sent
  * to the dev curation admin tool :D
- * 
+ *
  * Note that this class behaves differently based on the environment to which
  * it was deployed!
  */
@@ -39,16 +39,31 @@ export class ProspectEvents extends Resource {
     super(scope, name);
 
     // pre-existing queues (prod and dev) created by prospect-api
-    this.sqs = new sqs.DataAwsSqsQueue(this, `prospect-${config.environment}-queue`, {
-      name: `ProspectAPI-${config.environment}-Sqs-Translation-Queue`
-    })
+    this.sqs = new sqs.DataAwsSqsQueue(
+      this,
+      `prospect-${config.environment}-queue`,
+      {
+        name: `ProspectAPI-${config.environment}-Sqs-Translation-Queue`,
+      }
+    );
 
-    this.sqsDlq = new sqs.DataAwsSqsQueue(this, `prospect-${config.environment}-dlq`, {
-      name: `ProspectAPI-${config.environment}-Sqs-Translation-Queue-Deadletter`
+    this.sqsDlq = new sqs.DataAwsSqsQueue(
+      this,
+      `prospect-${config.environment}-dlq`,
+      {
+        name: `ProspectAPI-${config.environment}-Sqs-Translation-Queue-Deadletter`,
+      }
+    );
+
+    this.snsTopic = new sns.SnsTopic(this, 'prospect-event-topic', {
+      name: `${config.prefix}-ProspectEventTopic`,
     });
 
     this.createProspectEventRules();
     this.createPolicyForEventBridgeToSqs();
+
+    this.createDismissProspectEventRule();
+    this.createPolicyForEventBridgeToSns();
 
     // TODO: create a policy function for dev event bridge
     // this.createPolicyForEventBridgeToDevEventBridge();
@@ -62,10 +77,10 @@ export class ProspectEvents extends Resource {
     // both prod and dev have an sqs target
     const targets: PocketEventBridgeTargets[] = [
       {
-          arn: this.sqs.arn,
-          deadLetterArn: this.sqsDlq.arn,
-          targetId: `${config.prefix}-Prospect-Event-SQS-Target`,
-        },
+        arn: this.sqs.arn,
+        deadLetterArn: this.sqsDlq.arn,
+        targetId: `${config.prefix}-Prospect-Event-SQS-Target`,
+      },
     ];
 
     // only prod also targets the dev event bridge
@@ -75,8 +90,8 @@ export class ProspectEvents extends Resource {
       // allow it to receive events from the prod bus.
       targets.push({
         arn: 'arn:aws:events:us-east-1:410318598490:event-bus/PocketEventBridge-Dev-Shared-Event-Bus',
-        targetId: `${config.prefix}-Prospect-Event-Dev-Event-Bridget-Target`
-      })
+        targetId: `${config.prefix}-Prospect-Event-Dev-Event-Bridget-Target`,
+      });
     }
 
     const prospectEventRuleProps: PocketEventBridgeProps = {
@@ -96,6 +111,64 @@ export class ProspectEvents extends Resource {
       `${config.prefix}-Prospect-Prod-EventBridge-Rule`,
       prospectEventRuleProps
     );
+  }
+
+  //TODO: add comments
+  private createDismissProspectEventRule() {
+    const targets: PocketEventBridgeTargets[] = [
+      {
+        arn: this.snsTopic.arn,
+        deadLetterArn: this.sqsDlq.arn, //using the same DLQ for all prospect events
+        targetId: `${config.prefix}-Prospect-Event-SNS-Target`,
+        terraformResource: this.snsTopic,
+      },
+    ];
+
+    const prospectEventRuleProps: PocketEventBridgeProps = {
+      eventRule: {
+        name: `${config.prefix}-ProspectEvents-Rule`,
+        eventPattern: {
+          source: [eventConfig.dismissProspect.source],
+          'detail-type': eventConfig.dismissProspect.detailType,
+        },
+        eventBusName: this.sharedEventBus.bus.name,
+      },
+      targets,
+    };
+
+    new PocketEventBridgeRuleWithMultipleTargets(
+      this,
+      `${config.prefix}-Dismiss-Prospect-Prod-EventBridge-Rule`,
+      prospectEventRuleProps
+    );
+  }
+
+  //TODO: add comments
+  private createPolicyForEventBridgeToSns() {
+    const eventBridgeSnsPolicy = new iam.DataAwsIamPolicyDocument(
+      this,
+      `${config.prefix}-EventBridge-SNS-Policy`,
+      {
+        statement: [
+          {
+            effect: 'Allow',
+            actions: ['sns:Publish'],
+            resources: [this.snsTopic.arn],
+            principals: [
+              {
+                identifiers: ['events.amazonaws.com'],
+                type: 'Service',
+              },
+            ],
+          },
+        ],
+      }
+    ).json;
+
+    return new sns.SnsTopicPolicy(this, 'prospect-events-sns-topic-policy', {
+      arn: this.snsTopic.arn,
+      policy: eventBridgeSnsPolicy,
+    });
   }
 
   private createPolicyForEventBridgeToSqs() {
@@ -121,7 +194,7 @@ export class ProspectEvents extends Resource {
 
     new sqs.SqsQueuePolicy(this, 'prospect-events-sqs-policy', {
       policy: eventBridgeSqsPolicy,
-      queueUrl: this.sqs.url
+      queueUrl: this.sqs.url,
     });
   }
 }
